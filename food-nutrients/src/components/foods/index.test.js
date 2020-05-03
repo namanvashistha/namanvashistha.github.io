e09f3e6054579accf854da258721023bd306f477
namanvashistha <namanvashistha15@gmail.com>
2020-05-03 18:49:42 +0530

    del

--- food-nutrients/src/components/foods/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Foods from './index'
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Foods updateNutrients={() => {}} />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
