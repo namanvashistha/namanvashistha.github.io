7954b59f1e7f16e3f13a1283f7da636dce4dc4d0
namanvashistha <namanvashistha15@gmail.com>
2026-02-07 02:15:51 +0530

    refactor: remove unused node adapter and update giscus endpoint to use GET method

--- src/pages/styles/giscus.ts
+++ src/pages/styles/giscus.ts
@@ -1,8 +1,6 @@
-export const prerender = false;
-
 import css from '../../styles/giscus.css?raw';
 
-export async function ALL() {
+export async function GET() {
 	const res = new Response(css);
 
 	res.headers.set('Content-Type', 'text/css');
