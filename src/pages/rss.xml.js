e9fe738c1335cda47d7b03161944661f1bc2e478
namanvashistha <namanvashistha15@gmail.com>
2026-02-17 13:59:59 +0530

    feat: Add markdown-it and sanitize-html packages, updating dependency lock files.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('posts');
@@ -15,6 +19,9 @@ export async function GET(context) {
         pubDate: post.data.createdAt,
         description: post.data.description,
         link: `/blog/${post.id}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        }),
       })),
     customData: `<language>en-us</language>`,
   });
